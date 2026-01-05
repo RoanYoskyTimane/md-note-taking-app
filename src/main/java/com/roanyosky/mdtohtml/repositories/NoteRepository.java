ff0cb7a7be056ded1a2314411a4ebf4e25cb7432
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-05 19:27:51 +0200

    Create repository for the entity Note

---
+++ src/main/java/com/roanyosky/mdtohtml/repositories/NoteRepository.java
@@ -0,0 +1,9 @@
+package com.roanyosky.mdtohtml.repositories;
+
+import com.roanyosky.mdtohtml.entities.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoteRepository extends JpaRepository<Note,Integer> {
+}
