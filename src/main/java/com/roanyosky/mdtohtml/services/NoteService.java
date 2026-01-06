7d259789118644fe7b3e5ff4e8ffa63e4d6743e4
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-06 18:50:17 +0200

    Add the method service by filename

--- src/main/java/com/roanyosky/mdtohtml/services/NoteService.java
+++ src/main/java/com/roanyosky/mdtohtml/services/NoteService.java
@@ -2,11 +2,14 @@ package com.roanyosky.mdtohtml.services;
 
 import com.roanyosky.mdtohtml.dtos.NoteCreateDto;
 import com.roanyosky.mdtohtml.dtos.NoteDto;
+import com.roanyosky.mdtohtml.dtos.NoteUpdateDto;
 import com.roanyosky.mdtohtml.entities.Note;
 import com.roanyosky.mdtohtml.mappers.NoteMapper;
 import com.roanyosky.mdtohtml.repositories.NoteRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -14,9 +17,17 @@ public class NoteService {
     private final NoteMapper noteMapper;
     private final NoteRepository noteRepository;
 
+
     public NoteDto createNote(NoteCreateDto noteCreateDto)
     {
         Note newNote = noteMapper.toEntity(noteCreateDto);
         return noteMapper.toDto(noteRepository.save(newNote));
+    }
+
+    public NoteDto updateNote(NoteUpdateDto noteUpdateDto, String fileName)
+    {
+        Note noteToUpdate = noteRepository.findByFileName(fileName);
+        noteMapper.updateDto(noteUpdateDto, noteToUpdate);
+        return noteMapper.toDto(noteRepository.save(noteToUpdate));
     }
 }
