c6b8b8c01412a76188bbcd6834f77bb7ede73c9b
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-06 18:47:30 +0200

    Change the GenerateType to IDENTITY

--- src/main/java/com/roanyosky/mdtohtml/entities/Note.java
+++ src/main/java/com/roanyosky/mdtohtml/entities/Note.java
@@ -1,18 +1,35 @@
 package com.roanyosky.mdtohtml.entities;
 
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.Instant;
 
+@Entity
+@Table(name ="notes")
 @Getter
 @Setter
 @NoArgsConstructor
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "filename")
     private String fileName;
+
+    @Column(name = "content")
     private String content;
-    private Date createdAt;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+    }
 }
