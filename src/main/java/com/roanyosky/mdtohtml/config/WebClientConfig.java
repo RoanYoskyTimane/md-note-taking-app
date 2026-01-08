05e3cbc4ad783646943b2662c99edee20a1c50b3
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-08 16:10:31 +0200

    Add the @Bean annotation to the constructor

--- src/main/java/com/roanyosky/mdtohtml/config/WebClientConfig.java
+++ src/main/java/com/roanyosky/mdtohtml/config/WebClientConfig.java
@@ -1,6 +1,7 @@
 package com.roanyosky.mdtohtml.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -9,6 +10,7 @@ public class WebClientConfig {
     @Value("${language-tool.api-url}")
     String apiUrl;
 
+    @Bean
     public WebClient languageToolWebClient(){
         return WebClient.builder()
                 .baseUrl(apiUrl)
