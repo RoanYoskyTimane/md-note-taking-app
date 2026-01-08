aefc9adb284216bf122e62919be88a29aa4789df
Roan Yosky Timane <roanyosky@gmail.com>
2026-01-08 16:11:27 +0200

    Remove the commenteries from the main class

--- src/main/java/com/roanyosky/mdtohtml/MdtohtmlApplication.java
+++ src/main/java/com/roanyosky/mdtohtml/MdtohtmlApplication.java
@@ -19,19 +19,6 @@ public class MdtohtmlApplication {
 
 		dotenv.entries().forEach(entry -> {System.setProperty(entry.getKey(), entry.getValue());});
 		SpringApplication.run(MdtohtmlApplication.class, args);
-//		Note note = new Note();
-//		note.setId(1);
-//		note.setFileName("labulu");
-//		note.setContent("Labubu are the thing");
-//		System.out.println("The id:"+note.getId());
-//		System.out.println("the file name:"+note.getFileName());
-//
-//		Parser parser = Parser.builder().build();
-//		Node document = parser.parse("This is *Markdown*");
-//		HtmlRenderer renderer = HtmlRenderer.builder().build();
-//		String html = renderer.render(document);
-//		System.out.println(html);
-
 	}
 
 }
